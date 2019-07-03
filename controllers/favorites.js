e0ce130f408becceabf7578e1b1eb3b5640a36a3
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-07-03 11:58:29 -0700

    creating routes to link to the comments and display comments

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -38,46 +38,52 @@ router.post('/', function(req, res) { // appends to the first parameter in the i
 
 // GET /trail/:number - Gets one trail id from the database
 // and uses it to look up details about that one trail
-// router.get("/:number", function(req, res) {
-//   var trailUrl = 'https://www.hikingproject.com/data/get-trails-by-id?ids=' + req.params.number + '&key=' + process.env.HIKING_PROJECT_API;
-//   // Use request to call the API
-//   axios.get(trailUrl).then( function(apiResponse) {
-//       let trailDetails = apiResponse.data;
-//       res.render('favorites/show', { trailDetails });
-//   })
+router.get("/:number", function(req, res) {
+  var trailUrl = 'https://www.hikingproject.com/data/get-trails-by-id?ids=' + req.params.number + '&key=' + process.env.HIKING_PROJECT_API;
+  // Use request to call the API
+  axios.get(trailUrl).then( function(apiResponse) {
+      let trailDetails = apiResponse.data;
+      db.trail.findOne({
+        where: {number: parseInt(req.params.number)},
+        include: [db.comment, db.user]
+      }).then(function(trail) {
+        res.render('favorites/show', { trail, trailDetails });
+      })
+
+    })
   
 
-// });
+});
 
 // GET /trail/:number - Gets one trail id from the database and returns show page
 //TODO: Keep DARK SKY API HERE
-router.get('/:number', function(req, res) {
-  // TODO: Look up trail in our db by its trail ID which is in the number column (findOne)
-    var trailUrl = 'https://www.hikingproject.com/data/get-trails-by-id?ids=' + req.params.number + '&key=' + process.env.HIKING_PROJECT_API;
-    axios.get(trailUrl).then( function(apiResponse) {
-        let trailDetails = apiResponse.data;
-        let weatherRequest = trailDetails.trails.map( function (trail) {
-            return function(callback) {
-                let weatherUrl = 'https://api.darksky.net/forecast/' + process.env.DARK_SKY_API + '/' + trail.latitude + ',' + trail.longitude;
-                axios.get(weatherUrl).then( function (results) {
-                    let name = trail.name;
-                    let weather = results.data.daily.data.map( function(temp) {
-                        return temp.temperatureMax;
-                    })
-                    callback(null, {trail: name, weather})
-                })
-            }
-        })
+// router.get('/:number', function(req, res) {
+//   // TODO: Look up trail in our db by its trail ID which is in the number column (findOne)
+//     var trailUrl = 'https://www.hikingproject.com/data/get-trails-by-id?ids=' + req.params.number + '&key=' + process.env.HIKING_PROJECT_API;
+//     axios.get(trailUrl).then( function(apiResponse) {
+//         let trailDetails = apiResponse.data;
+//         let weatherRequest = trailDetails.trails.map( function (trail) {
+//             return function(callback) {
+//                 let weatherUrl = 'https://api.darksky.net/forecast/' + process.env.DARK_SKY_API + '/' + trail.latitude + ',' + trail.longitude;
+//                 axios.get(weatherUrl).then( function (results) {
+//                     let name = trail.name;
+//                     let weather = results.data.daily.data.map( function(temp) {
+//                         return temp.temperatureMax;
+//                     })
+//                     callback(null, {trail: name, weather})
+//                 })
+//             }
+//         })
     
-    async.parallel(async.reflectAll(weatherRequest), function(err, results) {
+//     async.parallel(async.reflectAll(weatherRequest), function(err, results) {
         
-        // res.json(results)
-        res.render('favorites/show', { trailDetails, results });
-    })
-    })  
+//         // res.json(results)
+//         res.render('favorites/show', { trailDetails, results });
+//     })
+//     })  
       
     
-});
+// });
 
 
 router.delete('/:number', function(req, res) {
@@ -90,5 +96,20 @@ router.delete('/:number', function(req, res) {
 
 // PUT /pokemon/:id
 // router.put("/:id")
+router.post("/:number/comments", function(req, res) { // wouldn't just post to :id because it wouldn't be a restful route
+    db.trail.findOne({
+      where: {number: parseInt(req.params.number)}
+     // either this way or (see below uncommented) // more secure since sequelize takes care of the relations
+    }).then(function(trail) {
+        trail.createComment({
+            userId: req.user.id,
+            comment: req.body.comment
+        }).then(function(comment) {
+            res.redirect("/trail/favorites/" + req.params.number) 
+        });
+    });
+});
+
+
 
 module.exports = router;
